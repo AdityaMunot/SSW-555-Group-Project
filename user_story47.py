79ad88e0af2efd22a7a5bb8700fab3395435e108
tagerjump <45743053+tagerjump@users.noreply.github.com>
2019-02-25 22:15:53 -0500

    Update user_story47.py

--- user_story47.py
+++ user_story47.py
@@ -1,6 +1,6 @@
 
 # User Story 4
-def Divorce_before_marriage(family_list):  # Check Divorce before death
+def Divorce_before_marriage(family_list):  # Check Divorce before marriage
     for i in family_list:
         if i[4] != "NA":
             if i[4] < i[3]:
@@ -9,7 +9,7 @@ def Divorce_before_marriage(family_list):  # Check Divorce before death
 
 # User Story 7
 
-def Less_then_150_years_old(family_list):  # Check Divorce before death
+def Less_then_150_years_old(family_list):  # Check Age more than 150
     for i in individual_list:
         calculate_age(i[3], i[4]) > 150:
             print(f"Error: {calculate_age(i[3], i[4])} is more than 150")
