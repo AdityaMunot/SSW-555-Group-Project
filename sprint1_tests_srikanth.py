078a509dc23f75c93cb337d1058ae5b47f844d15
srikii <44855671+srikii@users.noreply.github.com>
2019-02-25 21:06:17 -0500

    Update sprint1_tests_srikanth.py

--- sprint1_tests_srikanth.py
+++ sprint1_tests_srikanth.py
@@ -4,16 +4,16 @@ import unittest
 class TestSprint1(unittest.TestCase):
 
     def test_marriage_before_death_1(self):
-        self.assertEqual(marriage_before_death(family_list, individual_list), 0)
+        self.assertEqual(marriage_before_death(family_list, individual_list),([], " list of families who do not have marriage date before death date of one of the members"))
         self.assertNotEqual(marriage_before_death(family_list, individual_list), 1)
-        self.assertFalse(marriage_before_death(family_list, individual_list))
+        self.assertTrue(marriage_before_death(family_list, individual_list))
         self.assertIsNotNone(marriage_before_death(family_list, individual_list))
         self.assertIsNot(marriage_before_death(family_list, individual_list), '')
     
     def test_siblings_fewer_than_15(self):
-        self.assertEqual(fewer_than_15_siblings(family_list),0)
+        self.assertEqual(fewer_than_15_siblings(family_list), ([], " list of families have more than 15 siblings"))
         self.assertNotEqual(fewer_than_15_siblings(family_list),1)
-        self.assertFalse(fewer_than_15_siblings(family_list))
+        self.assertTrue(fewer_than_15_siblings(family_list))
         self.assertIsNotNone(fewer_than_15_siblings(family_list))
         self.assertIsNot(fewer_than_15_siblings(family_list), "")
 
