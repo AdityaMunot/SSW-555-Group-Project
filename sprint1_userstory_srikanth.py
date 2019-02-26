79a172d44f0b91dc4ed2266e359f59940cc5f7f1
srikii <44855671+srikii@users.noreply.github.com>
2019-02-25 21:13:41 -0500

    Add files via upload

---
+++ sprint1_userstory_srikanth.py
@@ -0,0 +1,20 @@
+from gedcom_parser import Gedreader
+
+def marriage_before_death(family_list, individual_list): #user story 05
+
+    l=list()
+    for i in individual_list:
+        if i[5] is not "NA" and i[4] is not "NA":
+            for j in family_list:
+                if i[5]== j[0] and i[0] == j[1]:
+                    if j[3] > i[4]:
+                        l.append(j[0])
+    return l, " list of families who do not have marriage date before death date of one of the members"
+
+def fewer_than_15_siblings(family_list): #user story 15
+    
+    l=list()
+    for i in family_list:
+        if len(i[5]) > 15:
+            l.append(i[0])
+    return l, " list of families have more than 15 siblings"
