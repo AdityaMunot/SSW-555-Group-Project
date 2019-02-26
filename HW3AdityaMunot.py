5e03b9ee3acc15d6f1da47380503957fc963b430
srikii <44855671+srikii@users.noreply.github.com>
2019-02-25 21:07:47 -0500

    Update HW3AdityaMunot.py

--- HW3AdityaMunot.py
+++ HW3AdityaMunot.py
@@ -189,26 +189,24 @@ for i in family_list:
 print(table)
 
 
-def marriage_before_death(family_list, individual_list): #user story 05_srikanth
+def marriage_before_death(family_list, individual_list): #user story 05
 
-	flag=0
-	for i in individual_list:
-		if i[5] is not "NA" and i[4] is not "NA":
-			for j in family_list:
-				if i[5]== j[0] and i[0] == j[1]:
-					if j[3] > i[4]:
-						print("marriage date greater than death death")
-						flag = 1
-	return flag
+    l=list()
+    for i in individual_list:
+        if i[5] is not "NA" and i[4] is not "NA":
+            for j in family_list:
+                if i[5]== j[0] and i[0] == j[1]:
+                    if j[3] > i[4]:
+                        l.append(j[0])
+    return l, " list of families who do not have marriage date before death date of one of the members"
 
-
-def fewer_than_15_siblings(family_list): #user story 15_srikanth
-
-	flag = 0
-	for i in family_list:
-		if len(i[5]) > 15:
-			flag+= 1
-	return(flag)
+def fewer_than_15_siblings(family_list): #user story 15
+    
+    l=list()
+    for i in family_list:
+        if len(i[5]) > 15:
+            l.append(i[0])
+    return l, " list of families have more than 15 siblings"
 
 
 # Error list for the Weird Values
