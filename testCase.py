38ab942da92e3a61759ac317bf0c74dcd44d123a
Aditya Munot <munot4@gmail.com>
2019-02-25 21:56:43 -0500

    update test

--- testCase.py
+++ testCase.py
@@ -31,6 +31,14 @@ class GedreaderTest(unittest.TestCase):
         for i in family_list:
             self.assertEqual(i[4], "NA")
 
+    def test_Marriage_before_divorce(self): # testCase for Check Marriage before death
+        for i in family_list:
+            if i[4] != "NA":
+                self.assertLess(datetime.strptime(i[3], "%Y %b %d"), datetime.strptime(i[4], "%Y %b %d"))
+
+    def test_lessthen150(self): # testCase for Check age less than 150
+        for i in individual_list:
+            self.assertLess(calculate_age(i[3], i[4]), 150)
 
 if __name__ == '__main__':
     unittest.main(exit=False, verbosity=2)
