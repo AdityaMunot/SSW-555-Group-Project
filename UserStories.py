be6341124b941c5d278c04ceb24517f2f0cc2d20
Shrey <vaityshrey@gmail.com>
2019-02-27 16:09:02 -0500

    update code

--- UserStories.py
+++ UserStories.py
@@ -123,6 +123,7 @@ def marriage_before_death(id, marriage, husbandid, wifeid):
 
 
 def fewer_than_15_siblings(family_list):  # user story 15
+
     l = list()
     for i in family_list:
         if len(i[5]) > 15:
@@ -130,6 +131,19 @@ def fewer_than_15_siblings(family_list):  # user story 15
     return l, " list of families have more than 15 siblings"
 
 # Running User Stories
+
+
+"""
+def marriage_before_death(family_list, individual_list): #user story 05
+    l=list()
+    for i in individual_list:
+        if i[5] is not "NA" and i[4] is not "NA":
+            for j in family_list:
+                if i[5]== j[0] and i[0] == j[1]:
+                    if j[3] > i[4]:
+                        l.append(j[0])
+    return l, " list of families who do not have marriage date before death date of one of the members"
+"""
 
 individual_list = CheckDates(individual_list)  # Running User Story 1
 family_list = CheckDates(family_list)  # Running User Story 1
--- sprint1_userstory_srikanth.py
+++ sprint1_userstory_srikanth.py
@@ -1,13 +1,3 @@
 from HW3AdityaMunot import Gedreader
 
-def marriage_before_death(family_list, individual_list): #user story 05
-    l=list()
-    for i in individual_list:
-        if i[5] is not "NA" and i[4] is not "NA":
-            for j in family_list:
-                if i[5]== j[0] and i[0] == j[1]:
-                    if j[3] > i[4]:
-                        l.append(j[0])
-    return l, " list of families who do not have marriage date before death date of one of the members"
-
 
