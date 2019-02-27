7c59e91b5b9407eec1f9e99e996094f148d68520
srikii <44855671+srikii@users.noreply.github.com>
2019-02-27 15:42:26 -0500

    Update testCase.py

--- testCase.py
+++ testCase.py
@@ -40,5 +40,20 @@ class GedreaderTest(unittest.TestCase):
         for i in individual_list:
             self.assertLess(calculate_age(i[3], i[4]), 150)
 
+    def test_marriage_before_death(self): #test case for user story 05, marriage before death
+        self.assertEqual(marriage_before_death(family_list, individual_list),([], " list of families who do not have marriage date before death date of one of the members"))
+        self.assertNotEqual(marriage_before_death(family_list, individual_list), 1)
+        self.assertTrue(marriage_before_death(family_list, individual_list))
+        self.assertIsNotNone(marriage_before_death(family_list, individual_list))
+        self.assertIsNot(marriage_before_death(family_list, individual_list), '')
+    
+    def test_siblings_fewer_than_15(self): #test case for user story 15, fewer than 15 siblings
+        self.assertEqual(fewer_than_15_siblings(family_list), ([], " list of families have more than 15 siblings"))
+        self.assertNotEqual(fewer_than_15_siblings(family_list),1)
+        self.assertTrue(fewer_than_15_siblings(family_list))
+        self.assertIsNotNone(fewer_than_15_siblings(family_list))
+        self.assertIsNot(fewer_than_15_siblings(family_list), "")
+
+
 if __name__ == '__main__':
     unittest.main(exit=False, verbosity=2)
