1c4a9961de43fd5f600cac4fe1f92b2bff81f20c
Shrey <vaityshrey@gmail.com>
2019-03-20 15:15:11 -0400

    update user stories

--- testCase.py
+++ testCase.py
@@ -29,11 +29,13 @@ class GedreaderTest(unittest.TestCase):
             if i[4] != "NA":
                 self.assertLess(datetime.strptime(i[4], "%Y %b %d"), today)
 
-    def test_Divorce_before_death(self):  # testCase for Check Divorce before death
+    # testCase for Check Divorce before death
+    def test_Divorce_before_death(self):
         for i in Checked_Div_bef_dea:
             self.assertEqual(i[4], "NA")
 
-    def test_Marriage_before_divorce(self):  # testCase for Check Marriage before death
+    # testCase for Check Marriage before death
+    def test_Marriage_before_divorce(self):
         for i in Checked_Mar_bef_div:
             if i[4] != "NA":
                 self.assertLess(datetime.strptime(
@@ -46,66 +48,26 @@ class GedreaderTest(unittest.TestCase):
                 self.assertLess(age, 150)
 
     # test case for user story 15, fewer than 15 siblings
-    def test_siblings_fewer_than_15(self):
+    """def test_siblings_fewer_than_15(self):
         self.assertEqual(fewer_than_15_siblings(family_list),
                          ([], " list of families have more than 15 siblings"))
         self.assertNotEqual(fewer_than_15_siblings(family_list), 1)
         self.assertTrue(fewer_than_15_siblings(family_list))
         self.assertIsNotNone(fewer_than_15_siblings(family_list))
-        self.assertIsNot(fewer_than_15_siblings(family_list), "")
+        self.assertIsNot(fewer_than_15_siblings(family_list), "")"""
 
+    class TestBirth(unittest.TestCase):
+        def test_wrong(self):
+            """for death before birth"""
+            self.assertEqual(birth_before_death(individual_list), [('Error: User', '@F4@', 'was dead before birth'),
+                                                                   ('Error: User', '@F1@', 'was dead before birth')])
 
-class TestBirth(unittest.TestCase):
-    def test_normal(self):
-        # havent added Errors like unbound and type cos we will be adding them in the future updates
-        self.assertEqual(birth_before_death(
-            "@I8@", "1906 AUG 15", "1956 FEB 11"), "1956 FEB 11")
-        self.assertNotEqual(birth_before_death(
-            "@I8@", "1956 FEB 11", "1906 AUG 15"), "1906 AUG 15")
+    class TestMarriage(unittest.TestCase):
+        def test_normal(self):
+            # havent added Errors like unbound and type cos we will be adding them in the future updates
+            self.assertEqual((birth_before_marriage(individual_list, family_list)), [
+                             ('Error: User', '@I5@', 'was dead before birth')])
 
-    def test_wrong(self):
-        self.assertEqual(birth_before_death(
-            "@I8@", "1956 FEB 11", "1906 AUG 15"), "NA")
-        self.assertNotEqual(birth_before_death(
-            "@I8@", "1906 AUG 15", "1956 FEB 11"), "NA")
-        self.assertTrue(birth_before_death(
-            "@I8@", "1956 FEB 11", "1906 AUG 15") == "NA")
-        self.assertFalse(birth_before_death(
-            "@I8@", "1906 AUG 15", "1956 FEB 11") == "NA")
-
-
-class TestMarriage(unittest.TestCase):
-    def test_normal(self):
-        # havent added Errors like unbound and type cos we will be adding them in the future updates
-        self.assertEqual(marriage_before_death(
-            "@F2@", "1993 APR 7", "@I4@", "@I5@"), "1993 APR 7")
-        self.assertNotEqual(marriage_before_death(
-            "@F2@", "2010 APR 7", "@I4@", "@I5@"), "2010 APR 7")
-
-    def test_wrong(self):
-        self.assertEqual(marriage_before_death(
-            "@F2@", "2010 APR 7", "@I4@", "@I5@"), "NA")
-        self.assertNotEqual(marriage_before_death(
-            "@F2@", "1993 APR 7", "@I4@", "@I5@"), "NA")
-        self.assertFalse(marriage_before_death(
-            "@F2@", "1993 APR 7", "@I4@", "@I5@") == "NA")
-
-
-class TestBirth(unittest.TestCase):
-    def test_normal(self):
-        # havent added Errors like unbound and type cos we will be adding them in the future updates
-        self.assertEqual(marraige_before_birth(
-            "@F2@", "1993 APR 7", "@I4@", "@I5@"), "NA")
-        self.assertNotEqual(marraige_before_birth(
-            "@F2@", "2010 APR 7", "@I4@", "@I5@"), "2010 APR 7")
-
-    def test_wrong(self):
-        self.assertEqual(marraige_before_birth(
-            "@F2@", "2010 APR 7", "@I4@", "@I5@"), "NA")
-        self.assertNotEqual(marraige_before_birth(
-            "@F2@", "1993 APR 7", "@I4@", "@I5@"), "1993 APR 7")
-        self.assertTrue(marraige_before_birth(
-            "@F2@", "1993 APR 7", "@I4@", "@I5@") == "NA")
 
 if __name__ == '__main__':
     unittest.main(exit=False, verbosity=2)
