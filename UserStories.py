29779635fe84fc07463098493e0da9652529730f
srikii <44855671+srikii@users.noreply.github.com>
2019-03-25 23:05:02 -0400

    Update UserStories.py

--- UserStories.py
+++ UserStories.py
@@ -225,6 +225,40 @@ def parents_not_too_old(family_list, individual_list):
                 error += 1
     return error
 
+def correct_gender_for_role(family_list, individual_list): #user story 21
+    for i in family_list:
+        for j in individual_list:
+            if i[1] == j[0]:
+                if j[2] !="M":
+                    return("invalid gender in family ", i[1])
+            if i[2] == j[0]:
+                if j[2] != "F":
+                    return("invalid gender in family ", i[1])
+    return "all gender roles in the families are correct" 
+
+
+
+def unique_ids(family_list, individual_list): #user story 22
+    uid=list()
+    fid=list()
+
+    for i in family_list:
+        if i[0] not in fid:
+            fid.append(i[0])
+        else:
+            return i[0], " not unique in family list"
+
+    for i in individual_list:
+        if i[0] not in uid:
+            uid.append(i[0])
+        else:
+            return i[0], " not unique in individual list"
+
+    return "all IDs unique in Family and Individual list" 
+
+
+
+
 
 checked_no_bigamy = no_bigamy(family_list, individual_list)
 checked_parents_not_too_old = parents_not_too_old(family_list, individual_list)
@@ -235,6 +269,9 @@ print(birth_before_death(individual_list))
 print(birth_before_marriage(individual_list, family_list))
 print(list(marriage_under_age_14()))
 print(list(parents_birth_before_death()))
+print(correct_gender_for_role(family_list, individual_list))
+print(unique_ids(family_list,individual_list))
+
 
 CheckedIndividuals = CheckDates(individual_list)  # Running User Story 1
 CheckedFamilylist = CheckDates(family_list)  # Running User Story 1
