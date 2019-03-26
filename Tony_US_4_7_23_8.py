90a73a05365aca007af1d40d7120e35c63b08efe
Tony Hawkson <45743053+tagerjump@users.noreply.github.com>
2019-03-26 17:13:02 -0400

    Update Tony_US_4_7_23_8.py

--- Tony_US_4_7_23_8.py
+++ Tony_US_4_7_23_8.py
@@ -15,17 +15,23 @@ def Less_then_150_years_old(individual_list):
 
 #User Story 23
 def Unique_name_and_birthday(individual_list):
+	str = ''
 	for i in individual_list:
 		for j in individual_list:
 			if i[0] != j[0]:
 				if i[1]== j[1] and i[3]== j[3]:
-					print(f"Error: {i[1]} Birth at {i[3]} have been duplicated ")
+					str = f"Error: {i[1]} Birth at {i[3]} have been duplicated "
+					print(str)
+	return str
 
 #User Story 8
 def Birth_before_mariage_of_parents(individual_list, family_list):
+	str = ''
 	for family in family_list:
 		for child in family[5]:
 			for i in individual_list:
 				if child == i[0]:
 					if i[3] < family[3]:
-						print(f"Error: {child} birth at {i[3]} before parents mariage at {family[3]}")
+						str = f"Error: {i[1]} Birth at {i[3]} have been duplicated "
+						print(str)
+	return str
