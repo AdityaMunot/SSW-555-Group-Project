059a221b3f2aa037d8a22d011b054d4dbbde473b
Aditya Munot <munot4@gmail.com>
2019-04-09 01:11:37 -0400

    reader file

--- HW3AdityaMunot.py
+++ HW3AdityaMunot.py
@@ -133,5 +133,5 @@ def Gedreader(path):  # parsing the gedcom file
 
 
 # Function calling
-individual_list, family_list = Gedreader(input("Enter GedCom File Location: "))
-#individual_list, family_list = Gedreader("My-Family-25-Mar-2019-960.ged")
+#individual_list, family_list = Gedreader(input("Enter GedCom File Location: "))
+individual_list, family_list = Gedreader("My-Family-25-Mar-2019-960.ged")
