f45ac73a27d75c0ca1658d06418132a25691727e
Aditya Munot <munot4@gmail.com>
2019-04-16 00:22:50 -0400

    update

--- UserStories.py
+++ UserStories.py
@@ -22,9 +22,9 @@ def CheckDates(Tlist):  # Check dates in gedcom file are before current date
 	return Tlist
 
 
-def Divorce_before_death(family_list):  # User Story 6
+def Divorce_before_death(Tfamily_list):  # User Story 6
 	# Check Divorce before death
-	for i in family_list:
+	for i in Tfamily_list:
 		if i[4] != "NA":
 			for j in individual_list:
 				if i[1] in j[0]:
@@ -34,11 +34,11 @@ def Divorce_before_death(family_list):  # User Story 6
 			if i[4] > hdeath and i[4] > wdeath:
 				print(f"Error: US6- Divorce date {i[4]} is After Death ")
 				i[4] = "NA"
-	return family_list
+	return Tfamily_list
 
 
-def Marriage_before_divorce(family_list):  # User Story 4
-	for i in family_list:
+def Marriage_before_divorce(Tfamily_list):  # User Story 4
+	for i in Tfamily_list:
 		if i[4] != "NA":
 			Mdate = datetime.strptime(i[3], "%Y %b %d")
 			Ddate = datetime.strptime(i[4], "%Y %b %d")
@@ -46,18 +46,18 @@ def Marriage_before_divorce(family_list):  # User Story 4
 				i[4] = "NA"
 				print(
 					f"Error: US4- Family {i[0]} is Divorce date {i[4]} is before Marriage ")
-	return family_list
+	return Tfamily_list
 
 
-def lessthen150(individual_list):  # User Story 7
-	for i in individual_list:
+def lessthen150(Tindividual_list):  # User Story 7
+	for i in Tindividual_list:
 		age = calculate_age(i[3], i[4])
 		if type(age) == int:
 			if (age > 150):
 				i[3] = "NA"
 				print(
 					f"Error: US7- Individual {i[0]} age is greater than 150 years ")
-	return individual_list
+	return Tindividual_list
 
 
 def marriage_before_death(family_list, individual_list):  # user story 05
